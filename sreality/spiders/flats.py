fe88fbe23747f3d613600024543dbdad783928cd
Vid Urh <vidurh48@gmail.com>
2023-08-02 18:41:25 +0200

    fix: :bug: Correct db setup and insertion

--- sreality/spiders/flats.py
+++ sreality/spiders/flats.py
@@ -1,7 +1,8 @@
 import scrapy
 import json
 import re
-
+import psycopg2
+import time
 class FlatsSpider(scrapy.Spider):
     name = "flats"
     
@@ -9,7 +10,28 @@ class FlatsSpider(scrapy.Spider):
     scraped_items = 0
     max_items_to_scrape = 500
     start_urls = ["https://www.sreality.cz/en/search/for-sale/apartments"]
-        
+    conn = None
+    
+    def __init__(self):
+        retries = 10
+        for i in range(retries):
+            try:
+                self.conn = psycopg2.connect(
+                    dbname="sreality",
+                    user="postgres",
+                    password="postgres",
+                    host="db",
+                    port="5432"
+                )
+                
+                self.cursor = self.conn.cursor()
+            except Exception as e:
+                pass
+            if self.conn:
+                break
+            else:
+                time.sleep(5)
+    
     def parse(self, response):
         for page in range(self.max_items_to_scrape//60 + 1):
             url = f"https://www.sreality.cz/api/en/v2/estates?category_main_cb=1&category_type_cb=1&page={page}&per_page=60&tms=1690911976383"
@@ -37,7 +59,10 @@ class FlatsSpider(scrapy.Spider):
                 "title": re.sub(r'[^\x00-\x7F]', '', title),
                 "img_url": img_url
             }
-            yield item_data
+            sql = "INSERT INTO flats (title, img_url) VALUES (%s, %s)"
+            data = (item_data["title"], item_data["img_url"])
+            self.cursor.execute(sql, data)
+            self.conn.commit()
             self.scraped_items += 1
             if self.scraped_items >= self.max_items_to_scrape:
                 break
